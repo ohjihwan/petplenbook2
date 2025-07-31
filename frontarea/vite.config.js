cee4e67df8aba1a0989241429ac15d715f5684fd
오지환 <ohjihwan@naver.com>
2025-07-31 17:31:48 +0900

    배포

--- frontarea/vite.config.js
+++ frontarea/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base : '/petplanbook2/', 
+	base : '/petplanbook2/',
 	plugins: [react()],
 })
